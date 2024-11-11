9e73a747a755271064b6c9be26ae3c8c7447942e
draihan <draihan@student.ubc.ca>
2024-11-10 20:15:07 -0800

    le finale

--- pages/popup/src/Popup.tsx
+++ pages/popup/src/Popup.tsx
@@ -5,10 +5,10 @@ import { useStorage, withErrorBoundary, withSuspense } from '@extension/shared';
 import { exampleThemeStorage } from '@extension/storage';
 import type { ComponentPropsWithoutRef } from 'react';
 import { useEffect, useRef, useState } from 'react';
-import React from 'react';
 
 import { useGetVibes } from './useGetVibes';
-import { getRandomSong, Genre } from './genres';
+import type { Genre } from './genres';
+import { getRandomSong } from './genres';
 
 const Popup = () => {
   const { pageHTML, screenshotUrl, musicRecommendation } = useGetVibes();
@@ -16,6 +16,126 @@ const Popup = () => {
   const theme = useStorage(exampleThemeStorage);
   const isLight = theme === 'light';
   const logo = isLight ? 'popup/logo_vertical.svg' : 'popup/logo_vertical_dark.svg';
+
+  const [showPlay, setPlay] = useState(true);
+  const [showMain, setShowMain] = useState(true);
+  const xsymbol = 'popup/xsymbol.svg';
+  const skip_button = 'popup/skip.svg';
+  const play_button = showPlay ? 'popup/play.svg' : 'popup/pause.svg';
+  const logo_active = 'popup/logo_active.svg';
+
+  const onImageClick = () => setShowMain(!showMain);
+  const onPlayPause = () => setPlay(!showPlay);
+
+  const Active = () => {
+    return (
+      // <div className={'App-active'}>
+      //   <img
+      //     style={{ position: 'absolute', top: '0%', left: '0%', width: '70px', height: 'autofill' }}
+      //     src={chrome.runtime.getURL(logo_active)}
+      //     className="logo-active"
+      //     alt="logo"
+      //   />
+      //   <button style={{ position: 'absolute', top: '5%', right: '5%' }} className="exit-button" onClick={onImageClick}>
+      //     <img src={chrome.runtime.getURL(xsymbol)} className="exit-button" alt="exit" />
+      //   </button>
+      //   <button
+      //     style={{ position: 'absolute', top: '75%', left: '50%', transform: 'translate(-50%, -50%)' }}
+      //     className="play-button"
+      //     onClick={onImageClick}>
+      //     <img src={chrome.runtime.getURL(play_button)} className="play-button" alt="play" />
+      //   </button>
+      //   <button
+      //     style={{ position: 'absolute', top: '75%', left: '50%', transform: 'translate(-50%, -50%)' }}
+      //     className="play-button"
+      //     onClick={onPlayPause}>
+      //     <img src={chrome.runtime.getURL(play_button)} className="play-button" alt="play" />
+      //   </button>
+      //   <button
+      //     style={{ position: 'absolute', top: '77%', left: '65%', transform: 'translate(-50%, -50%)' }}
+      //     className="skip-button"
+      //     onClick={onImageClick}>
+      //     <img src={chrome.runtime.getURL(skip_button)} className="skip-button" alt="skip" />
+      //   </button>
+      //   <div style={{ position: 'absolute', top: '59%', left: '5%' }} className="gradient-line"></div>
+      // </div>
+      <div className="w-full flex-row justify-center bg-black mb-10">
+        {/* header */}
+        <div className="w-full flex justify-between items-center">
+          <img
+            className="w-10 flex items-center "
+            src={chrome.runtime.getURL(logo_active)}
+            alt="csdcvsdv"
+            height={12}
+          />
+          <button className="exit-button" onClick={onImageClick}>
+            <img src={chrome.runtime.getURL(xsymbol)} className="exit-button" alt="exit" />
+          </button>
+        </div>
+
+        {/* body */}
+        <div className="w-full flex-row justify-center items-center ">
+          <p className="text-white font-light text-xl">{genre}</p>
+          <div className="justify-center items-center w-full">
+            <img
+              src={chrome.runtime.getURL('popup/coool.gif')}
+              alt="Your GIF"
+              height={50}
+              className="w-32 ml-[88px] justify-center items-center"
+            />
+          </div>
+          <AudioButton genre={genre}>Play Music!</AudioButton>
+          <div className="w-10 bg-black"></div>
+        </div>
+      </div>
+    );
+  };
+
+  const Main = () => {
+    return (
+      <header className={`App-header`}>
+        <h1 className="header">Aurora</h1>
+        <button className="image-button" onClick={onImageClick}>
+          <img src={chrome.runtime.getURL(logo_active)} className="image-button" alt="logo" />
+        </button>
+      </header>
+    );
+  };
+
+  const Demo = () => {
+    return (
+      <div className={`App ${isLight ? 'bg-slate-50' : 'bg-gray-800'} h-screen overflow-auto`}>
+        <header className={`App-header ${isLight ? 'text-gray-900' : 'text-gray-100'}`}>
+          {/* <ToggleButton>Toggle theme</ToggleButton> */}
+
+          <img src={chrome.runtime.getURL(logo)} className="App-logo" alt="logo" />
+        </header>
+
+        <AudioButton genre={genre}>Play Music!</AudioButton>
+
+        <div className="p-4">
+          {/* {pageHTML && (
+        <div className="mt-4">
+          <h3 className="font-bold">Extracted Page HTML:</h3>
+          <textarea value={pageHTML} readOnly rows={10} cols={30} className="w-full p-2 border" />
+        </div>
+      )} */}
+          {screenshotUrl && (
+            <div className="mt-4">
+              <h3 className="font-bold">Screenshot:</h3>
+              <img src={screenshotUrl} alt="Screenshot" style={{ maxWidth: '100%' }} />
+            </div>
+          )}
+          {musicRecommendation && (
+            <div className="mt-4">
+              <h3 className="font-bold">Music Recommendation:</h3>
+              <p>{musicRecommendation}</p>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  };
 
   // State to manage the current genre
   const [genre, setGenre] = useState<Genre>('sad'); // Initialize to 'sad' or any default genre
@@ -44,35 +164,10 @@ const Popup = () => {
   }, [musicRecommendation]);
 
   return (
-    <div className={`App ${isLight ? 'bg-slate-50' : 'bg-gray-800'} h-screen overflow-auto`}>
-      <header className={`App-header ${isLight ? 'text-gray-900' : 'text-gray-100'}`}>
-        {/* <ToggleButton>Toggle theme</ToggleButton> */}
-
-        <img src={chrome.runtime.getURL(logo)} className="App-logo" alt="logo" />
-      </header>
-
-      <AudioButton genre={genre}>Play Music!</AudioButton>
-
-      <div className="p-4">
-        {/* {pageHTML && (
-          <div className="mt-4">
-            <h3 className="font-bold">Extracted Page HTML:</h3>
-            <textarea value={pageHTML} readOnly rows={10} cols={30} className="w-full p-2 border" />
-          </div>
-        )} */}
-        {screenshotUrl && (
-          <div className="mt-4">
-            <h3 className="font-bold">Screenshot:</h3>
-            <img src={screenshotUrl} alt="Screenshot" style={{ maxWidth: '100%' }} />
-          </div>
-        )}
-        {musicRecommendation && (
-          <div className="mt-4">
-            <h3 className="font-bold">Music Recommendation:</h3>
-            <p>{musicRecommendation}</p>
-          </div>
-        )}
-      </div>
+    <div className={`App`}>
+      {/* Show the Active component initially */}
+      {/* <Demo /> */}
+      {showMain ? <Main /> : <Active />}
     </div>
   );
 };
@@ -149,10 +244,17 @@ const AudioButton = (props: ComponentPropsWithoutRef<'button'> & { genre: Genre 
     <div className="flex flex-col items-center space-y-4">
       <button
         className={
-          props.className + ' ' + 'font-bold py-2 px-6 rounded shadow hover:scale-105 transition-transform duration-200'
+          props.className +
+          ' ' +
+          'font-bold py-2 px-6 rounded shadow hover:scale-105 transition-transform duration-200 '
         }
         onClick={handlePlayPause}>
-        {isPlaying ? 'Pause' : 'Play'} {props.children}
+        <img
+          src={chrome.runtime.getURL(isPlaying ? 'popup/pause.svg' : 'popup/play.svg')}
+          alt={isPlaying ? 'Pause' : 'Play'}
+          width={40}
+          height={40}
+        />
       </button>
     </div>
   );
